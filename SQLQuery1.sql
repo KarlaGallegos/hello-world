c9137ddbe73ed0f2382c9c393a07ae26030f38d0
KarlaGallegos <77857013+KarlaGallegos@users.noreply.github.com>
2022-09-15 02:05:21 -0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿USE [Socio]
+GO
+
+DECLARE	@return_value Int
+
+EXEC	@return_value = [dbo].[SocAlta]
+		@Id = N'1',
+		@Nombre = N'Rosa',
+		@Apellido = N'Hernandez',
+		@Direccion = N'Calle #1',
+		@Telefono = N'88888888888'
+
+SELECT	@return_value as 'Return Value'
+
+GO
